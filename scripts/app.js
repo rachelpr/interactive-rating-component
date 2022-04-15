4bbfa80872f8f529b729f43cb01a69a4efe66b7a
Rachel PR <rplourder@gmail.com>
2022-04-15 15:31:34 -0300

    Add functionality for getting info of button click, and rendering the thanks portion

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,42 @@
 $(document).ready(function () {
   $(".thanks").hide();
 
-  $("button").click(function () {
-    let $numBtn = $(this).parents(".numbers").find(".numBtn");
+  $(".numBtn").on("click", function () {
+    $(".numBtn").removeClass("btnClicked")
+    $(this).addClass("btnClicked")
+    let $numButton = $(this).parents("button").find(".numBtn")
+    let rating = $(this).attr("value");
+    //console.log($(this).html())
 
-    $($numBtn).toggleClass("btnClicked");
   });
+
+
 
   $("#submitRating").on("click", function (event) {
+    $(".rating").empty();
     $(".thanks").show();
+    console.log($(".btnClicked").html())
   });
 });
+
+const createThanksElement = function (rating) {
+  let $thanks = $(`
+      <h3>
+        You selected #
+        <!-- Add rating here -->
+        out of 5
+      </h3>
+      <h3>Thank you!</h3>
+      <p>
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!
+      </p>
+    `);
+}
+
+const renderThanks = function (ratings) {
+  for (let rating of ratings) {
+    let newRating = createThanksElement(rating);
+    $("#thanks-container").append(thanks)
+  }
+}

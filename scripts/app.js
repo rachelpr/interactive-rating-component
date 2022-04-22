ece05839d1d1155a148e1f6e763adbe1f2d93f6b
Rachel PR <rplourder@gmail.com>
2022-04-21 21:37:20 -0300

    Add functionality to see rating when submit button clicked

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,26 @@
 $(document).ready(function () {
-  $(".thanks").hide();
+  //$(".thanks").hide();
+  let clickedNum = " ";
 
   $(".numBtn").on("click", function () {
-    $(".numBtn").removeClass("btnClicked")
-    $(this).addClass("btnClicked")
-    let $numButton = $(this).parents("button").find(".numBtn")
-    let rating = $(this).attr("value");
-    //console.log($(this).html())
-
+    $(".numBtn").removeClass("btnClicked");
+    $(this).addClass("btnClicked");
+    //let $numButton = $(this).parents("button").find(".numBtn");
+    //let rating = $(this).attr("value");
+    clickedNum = $(this).text();
+    console.log($(this).html());
   });
-
-
 
   $("#submitRating").on("click", function (event) {
-    $(".rating").empty();
+    $(".rating").hide();
     $(".thanks").show();
-    console.log($(".btnClicked").html())
+    //let $numButton = $(this).parents("button").find(".numBtn");
+
+    //$('.numbers').children.find('.numBtn')
+    console.log($(".numBtn.btnClicked").text());
+
+    //let rating = $(this).attr("value");
+    $("#selectedNum").html(clickedNum);
+    //console.log($(".btnClicked").html());
   });
 });
-
-const createThanksElement = function (rating) {
-  const $thanks = $(`<section class="thanks" id="thanks-container"></section>`);
-  let $thanksContent = $(`
-  <span>
-      <h3>
-        You selected #
-        <!-- Add rating here -->
-        out of 5
-      </h3>
-      <h3>Thank you!</h3>
-      <p>
-        We appreciate you taking the time to give a rating. If you ever need
-        more support, don’t hesitate to get in touch!
-      </p>
-  </span>`);
-  return $thanks.html($thanksContent);
-}
-
-const renderThanks = function (ratings) {
-  for (let rating of ratings) {
-    let newRating = createThanksElement(rating);
-    $("#thanks-container").append(thanks)
-  }
-}

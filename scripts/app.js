de4c7dd62582d17a85341e6673b6708c5f1eac85
Rachel PR <rplourder@gmail.com>
2022-05-10 22:24:10 -0300

    Remove some commented code

--- scripts/app.js
+++ scripts/app.js
@@ -5,22 +5,12 @@ $(document).ready(function () {
   $(".numBtn").on("click", function () {
     $(".numBtn").removeClass("btnClicked");
     $(this).addClass("btnClicked");
-    //let $numButton = $(this).parents("button").find(".numBtn");
-    //let rating = $(this).attr("value");
-    clickedNum = $(this).text(); 
-    //console.log($(this).html());
+    clickedNum = $(this).text();
   });
 
   $("#submitRating").on("click", function (event) {
     $(".rating").hide();
     $(".thanks").show();
-    //let $numButton = $(this).parents("button").find(".numBtn");
-
-    //$('.numbers').children.find('.numBtn')
-    console.log($(".numBtn.btnClicked").text());
-
-    //let rating = $(this).attr("value");
-    $("#selectedNum").html(clickedNum); 
-    //console.log($(".btnClicked").html());
+    $("#selectedNum").html(clickedNum);
   });
 });
